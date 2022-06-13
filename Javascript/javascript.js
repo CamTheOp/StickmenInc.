39843081ee34ef469f35db839c2e4fa2620ea2a2
CamTheOp <PablitoTheKid@gmail.com>
2022-06-13 02:24:56 -0400

    Initial Commit
    First commit

---
+++ Javascript/javascript.js
@@ -0,0 +1,18 @@
+function imgSlider(anything){
+                document.querySelector('.Man') .src = anything;
+            }
+
+            function changeCircleColor(color){
+                const circle = document.querySelector('.circle');
+                circle.style.background = color;
+            }
+
+            function changeSpanColor(color){
+                const span = document.querySelector('.span');
+                span.style.color = color;
+            }
+
+            function changeButtonColor(color) {
+                    const button = document.querySelector('.button');
+                    button.style.background = color;
+                }
